1068d28f972b10d9be181091916a4c8c31117b42
Maria <65084935+mmalodemf@users.noreply.github.com>
2025-01-03 09:39:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
+
 
 # Establish Snowflake session
 @st.cache_resource
